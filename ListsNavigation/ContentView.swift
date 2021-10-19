b50e0128ecc396646359d2c6a04020c362c81c78
yujung <handnew04@gmail.com>
2021-10-19 16:07:26 +0900

    Initial Commit

---
+++ ListsNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListsNavigation
+//
+//  Created by yujung on 2021/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListsNavigation/ListsNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsNavigationApp.swift
+//  ListsNavigation
+//
+//  Created by yujung on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct ListsNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
